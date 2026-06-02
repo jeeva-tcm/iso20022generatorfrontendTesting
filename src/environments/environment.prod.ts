0026fa3adfaaab126bdc4302c7d83b65100f2c1a
jeeva-tcm <jeeva.c@tickingminds.com>
2026-06-02 11:09:13 +0530

    changed the db credientials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,15 +5,15 @@ import { initializeApp } from "firebase/app";
 
 export const environment = {
     production: true,
-     apiBaseUrl: 'https://iso20022generatorbackend.onrender.com',
+     apiBaseUrl: 'https://iso20022generatorbackend-test.onrender.com',
     firebaseConfig: {
-       apiKey: "AIzaSyCWkQ7izNkRPM4gE1HISRPnwFOgm1WrHg4",
-  authDomain: "isovalidator2.firebaseapp.com",
-  projectId: "isovalidator2",
-  storageBucket: "isovalidator2.firebasestorage.app",
-  messagingSenderId: "653661877429",
-  appId: "1:653661877429:web:60861122e04af3b13c7afc",
-  measurementId: "G-6HWFM21RJD"
+       apiKey: "AIzaSyAdEupd1RQVI8y2SNot4IB8BBubzGT5QU8",
+  authDomain: "iso-validator.firebaseapp.com",
+  projectId: "iso-validator",
+  storageBucket: "iso-validator.firebasestorage.app",
+  messagingSenderId: "246882613767",
+  appId: "1:246882613767:web:9f9f4343a851eb01f888a2",
+  measurementId: "G-WG5ZDJM0DT"
     }
 };
 const app = initializeApp(environment.firebaseConfig);
